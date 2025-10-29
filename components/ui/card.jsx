9a229281b74dd0efc85455ae1da6033d1fe9c204
Caue Veroneze <caueveroneze@MacBook-Air-de-Caue.local>
2025-10-29 04:59:02 -0300

    atualização da página de dieta

--- components/ui/card.jsx
+++ components/ui/card.jsx
@@ -2,12 +2,14 @@ import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
-const Card = React.forwardRef(({ className, ...props }, ref) => (
+const Card = React.forwardRef(({ className, children, ...props }, ref) => (
   <div
     ref={ref}
     className={cn('rounded-xl border bg-card text-card-foreground shadow', className)}
     {...props}
-  />
+  >
+    {children}
+  </div>
 ))
 Card.displayName = 'Card'
 
